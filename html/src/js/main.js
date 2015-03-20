6e20875ada811f34c628f1b84bf823c8c3f930fc
WildCat <wildcat.name@gmail.com>
2015-03-20 23:36:18 +0800

    Add html files for topic content

---
+++ html/src/js/main.js
@@ -0,0 +1,17 @@
+$(function(){
+    $('.entities .post').each(function(){
+       var self = $(this);
+        console.log('each');
+
+        var favCount = self.find('.fav-count span').html();
+        console.log('fav count: ' + favCount);
+        if(parseInt(favCount) < 1) self.find('.fav-count').hide();
+
+        var appreciationCount = self.find('.appr-count span').html();
+        if(parseInt(appreciationCount) < 1) self.find('.appr-count').hide();
+    });
+
+    // remove all image style in content
+    $('.content img.imgly').css('max-width', '100%');
+
+});
